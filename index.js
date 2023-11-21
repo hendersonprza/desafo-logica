f7a377824009d77ded26dc02d16dba13866c6a19
hendersonprza <hendersoneev@gmail.com>
2023-11-21 14:02:28 -0300

    desafio completo

---
+++ index.js
@@ -0,0 +1,20 @@
+var heroi = prompt("Herói, por favor, digite o seu nome:\n");
+var xp = prompt("Qual é a sua XP agora?:\n")
+
+if(xp <= 1000){
+    console.log(`O Herói de nome ${heroi} está no nível Ferro`);
+}else if(xp > 1000 && xp <= 2000){
+    console.log(`O Herói de nome ${heroi} está no nível Bronze`);
+}else if(xp > 2000 && xp <= 5999){
+    console.log(`O Herói de nome ${heroi} está no nível Prata`);
+}else if(xp >= 6000 && xp <= 7000){
+    console.log(`O Herói de nome ${heroi} está no nível Ouro`);
+}else if(xp > 7000 && xp <= 8000){
+    console.log(`O Herói de nome ${heroi} está no nível Platina`);
+}else if(xp > 8000 && xp <= 9000){
+    console.log(`O Herói de nome ${heroi} está no nível Ascendente`);
+}else if(xp > 9000 && xp <= 10000){
+    console.log(`O Herói de nome ${heroi} está no nível Imortal`);
+}else if(xp >= 10001){
+    console.log(`O Herói de nome ${heroi} está no nível Radiante`);
+}else{console.log("XP Inválido ou não reconhecido")}
